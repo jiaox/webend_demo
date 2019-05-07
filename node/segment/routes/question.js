537e5cc602a605d5a5afc3fd2df42457a12c66b1
jiaox <jiaox@newcensoft.com.cn>
2019-05-07 21:18:14 +0800

    node commit

--- node/segment/routes/question.js
+++ node/segment/routes/question.js
@@ -1,13 +1,17 @@
 var express = require('express');  
 var router = express.Router();  
+var checkSession = require('../jsbean/CheckSession');
   
 router.all('/ask', function(req, res) {  
-      
-    loginbean = req.session.loginbean;  
-    if(loginbean==undefined){  
-        res.send("<script>alert('登录过期,请重新登录');location.href='/';</script>");  
-        return;  
-    }  
+      loginbean = checkSession.check(req,res);
+      if(!loginbean){
+        return;
+      }
+    // loginbean = req.session.loginbean;  
+    // if(loginbean==undefined){  
+    //     res.send("<script>alert('登录过期,请重新登录');location.href='/';</script>");  
+    //     return;  
+    // }  
     res.render('ask');  
 }) 
  
